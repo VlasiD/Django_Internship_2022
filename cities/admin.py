b32fd5b95bd17b64b7f0b7f2426c6fe1fab45ec8
Vladimir <vladimirsidenko11@gmail.com>
2022-01-23 16:54:35 +0200

    Added list_display for admin panel.

--- cities/admin.py
+++ cities/admin.py
@@ -3,10 +3,12 @@ from cities.models import Country, City
 
 
 class CountryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'iso', 'population')
     search_fields = ['name']
 
 
 class CityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'country', 'population', 'has_mcdonalds')
     search_fields = ['name']
     list_filter = ['country', 'has_mcdonalds']
 
