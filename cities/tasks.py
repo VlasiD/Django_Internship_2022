6a13632bf50d135df496a493875c15b111dfc52c
Vladimir <vladimirsidenko11@gmail.com>
2022-02-12 18:34:25 +0200

    added email template.

--- cities/tasks.py
+++ cities/tasks.py
@@ -1,27 +1,20 @@
 import smtplib
+from django.template.loader import render_to_string
 from Django_Internship_2022.celery import celery_app
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMultiAlternatives
 from Django_Internship_2022.config import email, password
 
 
 @celery_app.task
-def send_activation_notification(user_email):
-    print('1111111')
+def send_activation_notification(user_email, username):
     server = smtplib.SMTP("smtp.gmail.com", 587)
     server.set_debuglevel(1)
     server.ehlo()
     server.starttls()
     server.login(email, password)
-    send_mail(
-        'Thank you for registration',
-        'Hello, Thank you for joining us.',
-        email,
-        [user_email],
-        fail_silently=False
-    )
-    print('222222222')
 
-
-@celery_app.task
-def test(arg):
-    print(arg)
+    html_body = render_to_string("email/email.html", context={'username': username})
+    subject = 'Welcome to Country&City Service'
+    msg = EmailMultiAlternatives(subject=subject, from_email=email, to=[user_email])
+    msg.attach_alternative(html_body, "text/html")
+    msg.send()
--- cities/views.py
+++ cities/views.py
@@ -1,3 +1,4 @@
+from django.contrib.auth import authenticate, login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.mixins import LoginRequiredMixin
@@ -7,7 +8,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from cities.models import Country, City
 from cities.forms import CountryForm, CityForm, SearchForm, CustomUserCreationForm
-from cities.tasks import send_activation_notification, test
+from cities.tasks import send_activation_notification
 
 
 def home(request):
@@ -47,7 +48,6 @@ def country(request, pk):
         page_num = 1
     page = paginator.get_page(page_num)
     context = {'cities': cities, 'country': country, 'page': page, 'form': form}
-    test.delay(5555555555555555555555555555)
     return render(request, 'cities/country.html', context=context)
 
 
@@ -139,7 +139,11 @@ def register(request):
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST)
         if form.is_valid():
-            new_user = form.save()
-            send_activation_notification.delay(new_user.email)
+            form.save()
+            new_user = authenticate(username=form.cleaned_data['username'],
+                                    password=form.cleaned_data['password1'],
+                                    )
+            login(request, new_user)
+            send_activation_notification.delay(new_user.email, new_user.username)
             return redirect('countries')
     return render(request, 'account/register_user.html', context={'form': form})
